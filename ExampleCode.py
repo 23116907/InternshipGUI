6867d5341dee82487c72c9e91748c2aaec3a7e81
Shravan Suresh <23ShravanS@tas.edu.tw>
2021-07-16 18:28:54 +0800

    Made a few changes

--- ExampleCode.py
+++ ExampleCode.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import askdirectory
 import tksheet
-
+import random
 
 
 # This creates the main window.
@@ -10,10 +10,16 @@ root=Tk()
 root.configure(bg = "white")
 
 
-String_Var = StringVar()
+directory_name_str = StringVar()
+
+
+runtest_status_label_str = StringVar()
 
 def select_directory():
-    String_Var.set(askdirectory())
+    directory_name_str.set(askdirectory())
+
+randomize_velocity_checkbutton_bool = BooleanVar(value=False)
+randomize_position_checkbutton_bool = BooleanVar(value=False)
 
 
 # You create a table, and you add to "root".
@@ -21,15 +27,31 @@ sheet1 = tksheet.Sheet(root)
 sheet2 = tksheet.Sheet(root)
 sheet3 = tksheet.Sheet(root)
 
+
+def run_test_callback():
+    if not directory_name_str.get():
+        runtest_status_label_str.set("Error: Empty directory address.")
+        return
+    else:
+        runtest_status_label_str.set("")
+    sheet1_data = sheet1.get_sheet_data(return_copy = True, get_header = False, get_index = False)
+    if not sheet1_data:
+        runtest_status_label_str.set("Error: Please enter pattern data.")
+        return
+
+    if not randomize_velocity_checkbutton_bool.get() and not randomize_velocity_checkbutton_bool.get():
+        print("Velocity table and Position table are empty")
+
+
+
 # Divide root into a grid, tells where to place sheet in that grid
 
-clicked = StringVar()
-clicked.set("COM port list")
+velocities_randomize = StringVar()
+velocities_randomize.set("COM port list")
 
-clicked1 = StringVar()
-clicked1.set("COM port list")
+positions_randomize = StringVar()
+positions_randomize.set("COM port list")
 
-import random
 n = random.randint(1, 5)
 m = random.randint(1, 5)
 l = random.randint(1, 5)
@@ -47,10 +69,15 @@ cow2 = [2]
 cow3 = [3]
 cow4 = [4]
 
+dow1 = [1]
+dow2 = [2]
+dow3 = [3]
+dow4 = [4]
+
 # Here we put all rows together
 sheet1Data = [row1, row2, row3]
 sheet2data = [cow1, cow2, cow3, cow4]
-sheet3data = [cow1, cow2, cow3, cow4]
+sheet3data = [dow1, dow2, dow3, dow4]
 
 # Here you input the data to the table
 sheet1.set_sheet_data(sheet1Data)
@@ -69,20 +96,23 @@ positions_table_labels_frame.grid(column=2, row=4, columnspan=1)
 
 frm_entry=Frame(master=root)
 
-drop_down = OptionMenu(root, clicked, "Port1", "Port2")
+drop_down = OptionMenu(root, velocities_randomize, "Port1", "Port2")
 connect_button = Button(width=10, height=1, bg = "white", fg= "red", text="Connect")
 status_label = Label(width=10, text="Status")
-drop_down1 = OptionMenu(root, clicked1, "Port3", "Port4")
+drop_down1 = OptionMenu(root, positions_randomize, "Port3", "Port4")
 connect_button1 = Button(width=10, height=1, bg = "white", fg= "red", text="Connect")
 status_label1 = Label(width=10, text="Status")
 velocities_label = Label(velocities_table_frame, bg="white", text="Velocities to test")
 positions_label = Label(positions_table_top_frame, bg="white", text="Positions to test")
 pattern_Label = Label(bg="white", text="Pattern Table")
-c = Checkbutton(velocities_table_frame, text="Randomize")
-c2 = Checkbutton(positions_table_top_frame, bg="white", text="Randomize")
-runtest_button = Button(positions_table_labels_frame, width=10, height=1, bg="white", fg="black", text="Run Test")
+c = Checkbutton(velocities_table_frame, text="Randomize",variable=randomize_velocity_checkbutton_bool)
+c2 = Checkbutton(positions_table_top_frame, bg="white", text="Randomize",variable=randomize_position_checkbutton_bool)
+
+runtest_button = Button(positions_table_labels_frame, width=10, height=1, bg="white", fg="black", text="Run Test", command=run_test_callback)
+runtest_status_label = Label(positions_table_labels_frame, bg="white", textvariable=runtest_status_label_str)
 status_label2 = Label(positions_table_labels_frame, bg="white", text="Status")
 storage_folder_button = Button(width = 60, height =1, command = select_directory, bg="white", text="Storage folder address")
+storage_folder_label = Label(root, bg="white", textvariable=directory_name_str)
 
 
 drop_down.grid( sticky=W, column=0, row=0)
@@ -97,8 +127,11 @@ pattern_Label.grid(column=0, row=3, sticky=W)
 c.grid(sticky=W, column=1, row=0)
 c2.grid(sticky=W, column=1, row=0)
 runtest_button.grid(sticky=NE, column=0, row=0)
+runtest_status_label.grid(sticky=NE, column=0, row=1)
 status_label2.grid(sticky=NE, column=0, row=1)
-storage_folder_button.grid(column=0, row=5, sticky = NW)
+storage_folder_button.grid(column=2, row=5, sticky = NW)
+storage_folder_label.grid(column=0, row=5, columnspan=2, sticky = NW)
+
 
 
 sheet1.grid(sticky=W, column=0, row=4)
